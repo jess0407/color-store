6d27139ba402618bdf7ec8915e3100bc0fc83a93
Jessie Shi <jess0407@users.noreply.github.com>
2020-06-28 16:23:45 +0100

    add mixin

--- src/components/swatch.vue
+++ src/components/swatch.vue
@@ -20,13 +20,10 @@
 
 
 <script>
-function padZero(str, len) {
-  len = len || 2;
-  var zeros = new Array(len).join("0");
-  return (zeros + str).slice(-len);
-}
+import { colorutil } from "../mixin";
 export default {
   name: "Swatch",
+  mixins: [colorutil],
   data() {
     return {
       copied: false
@@ -47,31 +44,6 @@ export default {
       window.getSelection().removeAllRanges();
       this.copied = true;
       setTimeout(() => (this.copied = false), 2000);
-    },
-    invertColor(hex, bw) {
-      if (hex.indexOf("#") === 0) {
-        hex = hex.slice(1);
-      }
-      // convert 3-digit hex to 6-digits.
-      if (hex.length === 3) {
-        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-      }
-      if (hex.length !== 6) {
-        throw new Error("Invalid HEX color.");
-      }
-      var r = parseInt(hex.slice(0, 2), 16),
-        g = parseInt(hex.slice(2, 4), 16),
-        b = parseInt(hex.slice(4, 6), 16);
-      if (bw) {
-        // http://stackoverflow.com/a/3943023/112731
-        return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#FFFFFF";
-      }
-      // invert color components
-      r = (255 - r).toString(16);
-      g = (255 - g).toString(16);
-      b = (255 - b).toString(16);
-      // pad each with zeros and return
-      return "#" + padZero(r) + padZero(g) + padZero(b);
     }
   }
 };
